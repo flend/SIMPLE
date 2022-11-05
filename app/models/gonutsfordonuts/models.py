d3598893d0e3cb9bfea36e92b70d6088cffdd2d0
flend <flend@compsoc.net>
2022-11-05 08:12:58 +0000

    Starting SB3 dense resnet

--- app/models/gonutsfordonuts/models.py
+++ app/models/gonutsfordonuts/models.py
@@ -3,14 +3,97 @@ import tensorflow as tf
 tf.get_logger().setLevel('INFO')
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
-from tensorflow.keras.layers import BatchNormalization, Activation, Flatten, Add, Dense, Multiply, Concatenate, Lambda
-import tensorflow.keras.backend as K
-from stable_baselines.common.policies import ActorCriticPolicy
-from stable_baselines.common.distributions import CategoricalProbabilityDistribution
+import gym
+import torch as th
+import torch.nn as nn
 
+from stable_baselines3 import PPO
+from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 
 ACTIONS = 26
 FEATURE_SIZE = 94
+
+class CustomMaskedObsExtractor(BaseFeaturesExtractor):
+    """
+    :param observation_space: (gym.Space)
+    :param features_dim: (int) Number of features extracted.
+        This corresponds to the number of unit for the last layer.
+    """
+
+    def __init__(self, observation_space: gym.spaces.Box, features_dim: int = FEATURE_SIZE):
+        super(CustomMaskedObsExtractor, self).__init__(observation_space, features_dim)
+        
+    def forward(self, observations: th.Tensor) -> th.Tensor:
+        obs, legal_actions = split_input(observations, ACTIONS)
+        extracted_features = resnet_extractor(obs)
+        return extracted_features
+
+def resnet_extractor(y):
+    y = dense(y, FEATURE_SIZE)
+    y = residual(y, FEATURE_SIZE)
+    
+def residual(y, filters):
+    shortcut = y
+
+
+
+    y = dense(y, filters)
+    y = dense(y, filters, activation = None)
+    y = Add()([shortcut, y])
+    y = Activation('relu')(y)
+
+    return y
+
+def dense_only(y, filters, batch_norm = False, activation = 'relu', name = None):
+    
+
+
+def dense(y, filters, batch_norm = False, activation = 'relu', name = None):
+
+    if batch_norm or activation:
+        y = Dense(filters)(y)
+    else:
+        y = Dense(filters, name = name)(y)
+    
+    if batch_norm:
+        if activation:
+            y = BatchNormalization(momentum = 0.9)(y)
+        else:
+            y = BatchNormalization(momentum = 0.9, name = name)(y)
+
+    if activation:
+        y = Activation(activation, name = name)(y)
+    
+    return y
+
+
+
+
+
+
+
+
+        # We assume CxHxW images (channels first)
+        # Re-ordering will be done by pre-preprocessing or wrapper
+        n_input_channels = observation_space.shape[0]
+        self.cnn = nn.Sequential(
+            nn.Conv2d(n_input_channels, 32, kernel_size=8, stride=4, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=0),
+            nn.ReLU(),
+            nn.Flatten(),
+        )
+
+        # Compute shape by doing one forward pass
+        with th.no_grad():
+            n_flatten = self.cnn(
+                th.as_tensor(observation_space.sample()[None]).float()
+            ).shape[1]
+
+        self.linear = nn.Sequential(nn.Linear(n_flatten, features_dim), nn.ReLU())
+
+    def forward(self, observations: th.Tensor) -> th.Tensor:
+        return self.linear(self.cnn(observations))
 
 
 class CustomPolicy(ActorCriticPolicy):
